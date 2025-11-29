a479ab7fef1daa8725b1ed1b9c842a4a3ac85006
issraa <issraachkaydem802@gmail.com>
2025-11-29 12:04:16 -0800

    Remove BACK/hash.js from repository

--- BACK/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-async function generateHash() {
-  const password = "admin123456789"; // الباسوورد يلي بدك ياه
-  const hashed = await bcrypt.hash(password, 10);
-  console.log("Hashed password: ", hashed);
-}
-
-generateHash();
